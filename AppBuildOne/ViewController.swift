f2cb47e4170a92b0686ea73dc325030eb7af8ff1
KO-d14 <ainajk@hawaii.edu>
2023-09-26 11:21:26 +0900

    Initial Commit

---
+++ AppBuildOne/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppBuildOne
+//
+//  Created by Aina Kodaira on 9/26/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
